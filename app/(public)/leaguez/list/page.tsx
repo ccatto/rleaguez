9308d0c64634aa44758b2fa35efebb56c4c857ff
Catto <catto@live.com>
2024-06-26 15:23:13 -0400

    added sample data table.

--- app/(public)/leaguez/list/page.tsx
+++ app/(public)/leaguez/list/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next';
 import React from 'react';
 import Link from 'next/link';
+// import { schedule-table } from "."
+import { DataTable } from '../../../components/AtomicDesign/organisms/scheduleTable/schedule-table';
+import { PagesColumns } from '../../../components/AtomicDesign/organisms/scheduleTable/schedule-columns';
 
 export const metadata: Metadata = {
   title: 'RLeaguez Sports Leaguez',
@@ -18,10 +21,8 @@ export default function Page() {
       <br />
       <br />
 
-      
 
-
-      {/* <button
+      < DataTable columns={PagesColumns}  />
         type="button"
         className="mb-2 me-2 rounded-2xl bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
---
+++ app/(public)/testtable/zzztableColumns copy.tsx
@@ -0,0 +1,61 @@
+// 'use client';
+
+// import { ColumnDef } from '@tanstack/react-table';
+// import { MoreHorizontal } from 'lucide-react';
+
+// import { Button } from '../../../components/ui/button';
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuLabel,
+//   DropdownMenuSeparator,
+//   DropdownMenuTrigger,
+// } from '../../../components/ui/dropdown-menu';
+
+// // import { League } from '@/app/lib/definitions';
+// // import { League } from '../../../../../lib/definitions';
+
+// // import type:
+// import Person from './data';
+
+// // export const PagesColumns: ColumnDef<Person>[] = [
+ 
+
+
+// // const columnHelper = createColumnHelper<Person>()
+
+// // const columns = [
+// export const PagesColumns: ColumnDef<Person>[] = [
+//   {
+//     accessorKey: 'firstName',
+//     header: 'firstName',
+//   },
+//   columnHelper.accessor('firstName', {
+//     cell: info => info.getValue(),
+//     footer: info => info.column.id,
+//   }),
+//   columnHelper.accessor(row => row.lastName, {
+//     id: 'lastName',
+//     cell: info => <i>{info.getValue()}</i>,
+//     header: () => <span>Last Name</span>,
+//     footer: info => info.column.id,
+//   }),
+//   columnHelper.accessor('age', {
+//     header: () => 'Age',
+//     cell: info => info.renderValue(),
+//     footer: info => info.column.id,
+//   }),
+//   columnHelper.accessor('visits', {
+//     header: () => <span>Visits</span>,
+//     footer: info => info.column.id,
+//   }),
+//   columnHelper.accessor('status', {
+//     header: 'Status',
+//     footer: info => info.column.id,
+//   }),
+//   columnHelper.accessor('progress', {
+//     header: 'Profile Progress',
+//     footer: info => info.column.id,
+//   }),
+// ]
